adb22afafcb9d2c4bc05ff47cc440ae198237d8d
Nabil Mahmud <nabilbndev@outlook.com>
2024-05-17 12:09:57 +0600

    fix: padding and font size in footer

--- app/_components/common/Footer.tsx
+++ app/_components/common/Footer.tsx
@@ -3,12 +3,12 @@ export default function Footer() {
   const date = new Date()
   const year = date.getFullYear();
   return (
-    <footer className=" flex text-sm md:text-base flex-col gap-2 items-center justify-center h-60 bg-slate-600 text-white t">
+    <footer className=" flex flex-col gap-2 items-center justify-center py-16 bg-slate-600 text-white t">
 
 
-      <p className=" text-center">Designed and developed with ❤️ for the teachers and students</p>
-      <p className="text-center">Hosted by vercel</p>
-      <p className="text-center">Copyright ©{year} English Dept. AHC</p>
+      <p className=" text-sm md:text-base text-center">Designed and developed with ❤️ for the teachers and students</p>
+      <p className="text-sm md:text-base text-center">Hosted by vercel</p>
+      <p className="text-sm md:text-base text-center">Copyright ©{year} English Dept. AHC</p>
 
 
     </footer>
