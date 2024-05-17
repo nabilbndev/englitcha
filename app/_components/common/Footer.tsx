5fb97763d0ae73eaa1acd94f378863446f9a02f7
Nabil Mahmud <nabilbndev@outlook.com>
2024-05-17 12:15:02 +0600

    fix: footer font size one more time

--- app/_components/common/Footer.tsx
+++ app/_components/common/Footer.tsx
@@ -6,9 +6,9 @@ export default function Footer() {
     <footer className=" flex flex-col gap-2 items-center justify-center py-16 bg-slate-600 text-white t">
 
 
-      <p className=" text-sm md:text-base text-center">Designed and developed with ❤️ for the teachers and students</p>
-      <p className="text-sm md:text-base text-center">Hosted by vercel</p>
-      <p className="text-sm md:text-base text-center">Copyright ©{year} English Dept. AHC</p>
+      <p className=" text-xs md:text-base text-center">Designed and developed with ❤️ for the teachers and students</p>
+      <p className=" text-xs md:text-base text-center">Hosted by vercel</p>
+      <p className=" text-xs md:text-base text-center">Copyright ©{year} English Dept. AHC</p>
 
 
     </footer>
