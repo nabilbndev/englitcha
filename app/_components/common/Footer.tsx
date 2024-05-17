cef657b85f26e10e0023279e4f092effcd74a73c
Nabil Mahmud <nabilbndev@outlook.com>
2024-05-17 12:04:11 +0600

    fix: footer text alignment

--- app/_components/common/Footer.tsx
+++ app/_components/common/Footer.tsx
@@ -3,12 +3,12 @@ export default function Footer() {
   const date = new Date()
   const year = date.getFullYear();
   return (
-    <footer className=" flex text-sm md:text-base flex-col gap-2 items-center justify-center h-60 bg-slate-600 text-white">
+    <footer className=" flex text-sm md:text-base flex-col gap-2 items-center justify-center h-60 bg-slate-600 text-white t">
 
 
-      <p>Designed and developed with ❤️ for the teachers and students</p>
-      <p>Hosted by vercel</p>
-      <p>Copyright ©{year} English Dept. AHC</p>
+      <p className=" text-center">Designed and developed with ❤️ for the teachers and students</p>
+      <p className="text-center">Hosted by vercel</p>
+      <p className="text-center">Copyright ©{year} English Dept. AHC</p>
 
 
     </footer>
